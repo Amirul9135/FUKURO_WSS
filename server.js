fda55910d794dc29646a16c6a9279d6201fafc1d
Amirul Asraf <amirul.asraf9135@gmail.com>
2023-06-05 15:22:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use("/api/node", require("./Controller/REST/NodeAPI"))
 app.use("/api/user", require("./Controller/REST/UserAPI"))
 app.use("/test", require("./Controller/REST/TestAPI"))
 
+app.get("/api/test", (req, res) => {
+    return res.send(new Date().toDateString())
+})
+
 app.get("/", function (req, res) {
     return res.status(200).send("test");
 })
